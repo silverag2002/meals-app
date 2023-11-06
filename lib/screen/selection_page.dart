8497b571216e5ef5e6c857d2b4f1bd49e7b2d760
silverag2002 <silverag2002@gmail.com>
2023-11-06 21:00:25 +0530

    Design for category picker page done

---
+++ lib/screen/selection_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+
+class SelectionScreen extends StatelessWidget {
+  SelectionScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Pick your Category"),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(20),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            for (final category in availableCategories)
+              InkWell(
+                  onTap: () {
+                    print(category.title);
+                  },
+                  splashColor: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(16),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          gradient: LinearGradient(
+                              colors: [
+                                category.color.withOpacity(0.55),
+                                category.color.withOpacity(0.9)
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight)
+                          // borderRadius: BorderRadius.circular(16)),
+                          ),
+                      padding: const EdgeInsets.all(16),
+                      child: Text(
+                        category.title,
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.onBackground),
+                      ))),
+          ],
+        ));
+  }
+}
