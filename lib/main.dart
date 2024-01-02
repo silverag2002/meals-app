9807426692873e20c4f729d8ee85bc29860da039
silverag2002 <silverag2002@gmail.com>
2024-01-02 22:08:05 +0530

    Restructured code

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo', theme: theme, home: SelectionScreen());
+      title: 'Flutter Demo',
+      theme: theme,
+      home: SelectionScreen(),
+    );
   }
 }
--- lib/screen/selection_page.dart
+++ lib/screen/selection_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screen/dashboard.dart';
 import 'package:meals_app/screen/recepie.dart';
 
 class SelectionScreen extends StatelessWidget {
@@ -7,53 +8,18 @@ class SelectionScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    void navigateToScreen(cat) {
-      var dMeals = dummyMeals
-          .where((element) => element.categories.contains(cat.id))
-          .toList();
-      print("LENGTH ${dMeals.length}");
-
-      Navigator.of(context).push(MaterialPageRoute(
-          builder: (ctx) => Recepie(title: cat.title, data: dMeals)));
-    }
+    final _selcetedNavItem = 0;
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Pick your Category"),
-        ),
-        body: GridView(
-          padding: const EdgeInsets.all(20),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: [
-            for (final category in availableCategories)
-              InkWell(
-                  onTap: () {
-                    navigateToScreen(category);
-                  },
-                  splashColor: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(16),
-                  child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          gradient: LinearGradient(
-                              colors: [
-                                category.color.withOpacity(0.55),
-                                category.color.withOpacity(0.9)
-                              ],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight)),
-                      padding: const EdgeInsets.all(16),
-                      child: Text(
-                        category.title,
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.onBackground),
-                      ))),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text("Pick your Category"),
+      ),
+      body: Dashboard(),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal), label: "Categories"),
+        BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites")
+      ]),
+    );
   }
 }
